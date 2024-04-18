96bcbd0758ff909b3a046b647089e3430b6e159b
Manuel Ramirez <manramloz@gmail.com>
2024-04-18 12:32:14 +0200

    bloquear chats si no hay token

--- assets/js/webModules/mis-chats/index.js
+++ assets/js/webModules/mis-chats/index.js
@@ -1,6 +1,12 @@
 import { misChatsController } from './misChatsController.js';
 import { sessionController } from '../session/sessionController.js';
 
+const token = localStorage.getItem('token');
+
+if (!token) {
+  window.location.href = '/login.html';
+}
+
 const enviarMensajeFormData = document.querySelector('#enviarMensajeForm');
 const sessionNav = document.getElementById('session');
 
